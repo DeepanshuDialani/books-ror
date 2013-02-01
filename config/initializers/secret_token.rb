ba09a97d69e4710ecea0ab4692064e7a272c686d
DeepanshuDialani <deepdialani42@gmail.com>
2013-02-01 18:43:19 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shelfari::Application.config.secret_token = '88713666e0d154c87944d023ee316b53a1d548e6b38bac0b3f596f1f33764c7fa1caf98bf0039d74c9a96ac76926f0c62cd810c020cfddc5cbee0304aa1aebf0'
